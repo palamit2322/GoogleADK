3c8d8eb65d6dcc9d871a643c791f91f49e7b401a
Amit Pal <amitpal@Amits-MacBook-Pro.local>
2026-06-14 10:13:34 +0530

    added working code

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -1,13 +1,11 @@
 from google.adk.agents import Agent
 from google.adk.tools import google_search_tool
-# from dotenv import load_dotenv
 
-#load_dotenv()
 
 root_agent=Agent(
-    name='QA_agent',
-    model='gpt-4o-mini',
+    name='QA_Agent',
+    model='gemini-2.5-flash',
     description='This agent is for generating the response based on your query',
     instruction='You are helpful assistant, Answer the query in polite and in short summary with in points if required',
-    tools=[google_search_tool]
+    #tools=[google_search_tool]
 )
