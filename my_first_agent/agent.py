77c4cfb566511eba9ed33e54c9400a064816b25f
Amit Pal <amitpal@Amits-MacBook-Pro.local>
2026-06-14 18:59:09 +0530

    added function tool

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -1,11 +1,25 @@
 from google.adk.agents import Agent
-from google.adk.tools import google_search_tool
 
+def get_weather_tool(city:str)->dict:
+    """
+    Description:
+    'This fucntion tool retrieves the weather details of the particular city'
+
+    Args:
+        city: It takes input as city of type string to get weather for.
+    Returns:
+        It returns a dictionary conatining the weather status and temperature    
+    
+    """
+    return {
+        "status":"success",
+        "Temperature":" The temperature is not available for the current city"
+    }
 
 root_agent=Agent(
     name='QA_Agent',
     model='gemini-2.5-flash',
     description='This agent is for generating the response based on your query',
     instruction='You are helpful assistant, Answer the query in polite and in short summary with in points if required',
-    #tools=[google_search_tool]
+    tools=[get_weather_tool]
 )
